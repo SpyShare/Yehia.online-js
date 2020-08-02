cf98651fac3031aa8f9c228a6c429bdb80e835f6
Spy Share <66829665+SpyShare@users.noreply.github.com>
2020-08-02 11:10:50 -0700

    Create 3hht.js

---
+++ 3hht.js
@@ -0,0 +1,50 @@
+$(document).keydown(function(e){
+      if( e.which === 89 && e.ctrlKey ){
+
+        location.href = 'https://'+window.location.hostname;
+
+      }
+
+      else if( e.which === 81 && e.ctrlKey ){
+
+        location.href= 'https://'+location.hostname+'/p/reuse.html';
+      }          
+}); 
+$("img").mousedown(function(){
+    return false;
+});
+var i = document.getElementById(&quot;menu&quot;).style;
+if (document.addEventListener) {
+  document.addEventListener(&#39;contextmenu&#39;, function(e) {
+    var posX = e.clientX;
+    var posY = e.clientY;
+    menu(posX, posY);
+    e.preventDefault();
+  }, false);
+  document.addEventListener(&#39;click&#39;, function(e) {
+    i.opacity = &quot;0&quot;;
+    setTimeout(function() {
+      i.visibility = &quot;hidden&quot;;
+    }, 501);
+  }, false);
+} else {
+  document.attachEvent(&#39;oncontextmenu&#39;, function(e) {
+    var posX = e.clientX;
+    var posY = e.clientY;
+    menu(posX, posY);
+    e.preventDefault();
+  });
+  document.attachEvent(&#39;onclick&#39;, function(e) {
+    i.opacity = &quot;0&quot;;
+    setTimeout(function() {
+      i.visibility = &quot;hidden&quot;;
+    }, 501);
+  });
+}
+
+function menu(x, y) {
+  i.top = y + &quot;px&quot;;
+  i.left = x + &quot;px&quot;;
+  i.visibility = &quot;visible&quot;;
+  i.opacity = &quot;1&quot;;
+}

fa1c085ebc2641e9c109ccd9eea9dcd1f8ba128c
Spy Share <66829665+SpyShare@users.noreply.github.com>
2020-08-03 08:07:22 -0700

    Update maph.php

--- maph.php
+++ maph.php
@@ -1,5 +1,8 @@
+<?php
+
 $homepage = file_get_contents('https://www.yehia.online/');
 $doc = new DOMDocument;
 $doc->loadHTML($homepage);
 $titles = $doc->getElementsByTagName('h3');
 echo $titles->item(0)->nodeValue;
+?>

a6f69344b28d1dfc2221ea4f05050f9d5b13f4e9
Spy Share <66829665+SpyShare@users.noreply.github.com>
2020-08-03 09:01:05 -0700

    Delete maph.php

--- maph.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$homepage = file_get_contents('https://www.yehia.online/');
-$doc = new DOMDocument;
-$doc->loadHTML($homepage);
-$titles = $doc->getElementsByTagName('h3');
-echo $titles->item(0)->nodeValue;
-?>

a9f7d37bd07f9163d1b7242e38f52add5becb755
Spy Share <66829665+SpyShare@users.noreply.github.com>
2020-08-11 04:44:45 -0700

    Create fed.js

---
+++ fed.js
@@ -0,0 +1,3 @@
+ var xa = "https://www.blogger.com/feeds/"+person+"/posts/default?alt=json-in-script&max-results=1&callback=handleResponse";
+
+document.write("<script type='text/javascript' src='"+ xa + "'><\/scr" + "ipt>");

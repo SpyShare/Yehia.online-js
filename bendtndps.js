d0090626ca0c067244653f87e65ba79a28bed48a
Spy Share <66829665+SpyShare@users.noreply.github.com>
2020-08-12 12:24:17 -0700

    Create bendtndps.js

---
+++ bendtndps.js
@@ -0,0 +1,35 @@
+  function handleResponse(response) {
+  	var post = response.feed.entry[0];
+
+   
+   
+   
+   
+       var canioa = post.content.$t.replace(/<[^>]+>/g, '').replace(/ /gi,"");
+
+    $('#myBtaan').html('\n'+canioa+'\n');
+        $('#contenaat').html('\n'+canioa+'\n');
+     
+    document.getElementById("text").style.height = 400;
+
+str = canioa;
+   
+   
+   
+   
+   
+    var countpsy = document.getElementById("endposta").innerHTML =post.title.$t;
+
+if (countpsy.length >= '18'){
+
+document.getElementById("endpswh").innerHTML = 'رائع العنوان عدد الأحرف مناسب'
+ document.getElementById("endpswh").style.color = 'green'
+if (countpsy.length <= '20'){
+
+document.getElementById("endpswh").innerHTML = 'سئ عدد أحرف العنوان قصيرة'
+ document.getElementById("endpswh").style.color = 'red'
+}
+
+}
+
+      }

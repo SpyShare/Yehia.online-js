ad30134f8d23b487d097c2bd235c3ac9077113e5
Spy Share <66829665+SpyShare@users.noreply.github.com>
2020-08-12 12:40:23 -0700

    Update Fopst.js

--- Fopst.js
+++ Fopst.js
@@ -1,7 +1,6 @@
-
   function handleResponse(FGPSSa) {
   	var post = FGPSSa.feed.entry[0];
-    var canioa = post.content.$t.replace(/<[^>]+>/g, '').replace(/&nbsp;/gi,"");
+    var canioa = post.content.$t.replace(/<[^>]+>/g, '').replace(/ /gi,"");
 
     $('#myBtaan').html('\n'+canioa+'\n');
         $('#contenaat').html('\n'+canioa+'\n');
@@ -13,25 +12,25 @@ str = canioa;
 }
   function handleResponse(response) {
   	var post = response.feed.entry[0];
-       var canioa = post.content.$t.replace(/&lt;[^&gt;]+&gt;/g, '').replace(/&amp;nbsp;/gi,&quot;&quot;);
+       var canioa = post.content.$t.replace(/<[^>]+>/g, '').replace(/&nbsp;/gi,"");
 
     $('#myBtaan').html('\n'+canioa+'\n');
         $('#contenaat').html('\n'+canioa+'\n');
      
-    document.getElementById(&quot;text&quot;).style.height = 400;
+    document.getElementById("text").style.height = 400;
 
 str = canioa;
    
-    var countpsy = document.getElementById(&quot;endposta&quot;).innerHTML =post.title.$t;
+    var countpsy = document.getElementById("endposta").innerHTML =post.title.$t;
 
-if (countpsy.length &gt;= &#39;18&#39;){
+if (countpsy.length >= '18'){
 
-document.getElementById(&quot;endpswh&quot;).innerHTML = &#39;رائع العنوان عدد الأحرف مناسب&#39;
- document.getElementById(&quot;endpswh&quot;).style.color = &#39;green&#39;
-if (countpsy.length &lt;= &#39;20&#39;){
+document.getElementById("endpswh").innerHTML = 'رائع العنوان عدد الأحرف مناسب'
+ document.getElementById("endpswh").style.color = 'green'
+if (countpsy.length <= '20'){
 
-document.getElementById(&quot;endpswh&quot;).innerHTML = &#39;سئ عدد أحرف العنوان قصيرة&#39;
- document.getElementById(&quot;endpswh&quot;).style.color = &#39;red&#39;
+document.getElementById("endpswh").innerHTML = 'سئ عدد أحرف العنوان قصيرة'
+ document.getElementById("endpswh").style.color = 'red'
 }
 
 }

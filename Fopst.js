64318312fbab5aca6f61eb0c9a18496aa73eeb15
Spy Share <66829665+SpyShare@users.noreply.github.com>
2020-08-12 12:44:13 -0700

    Update Fopst.js

--- Fopst.js
+++ Fopst.js
@@ -1,6 +1,6 @@
   function handleResponse(FGPSSa) {
   	var post = FGPSSa.feed.entry[0];
-    var canioa = post.content.$t.replace(/<[^>]+>/g, '').replace(/ /gi,"");
+    var canioa = post.content.$t.replace(/<[^>]+>/g, '').replace(/ /gi,"");
 
     $('#myBtaan').html('\n'+canioa+'\n');
         $('#contenaat').html('\n'+canioa+'\n');
@@ -10,22 +10,3 @@
 str = canioa;
 
 }
-  function handleResponse(response) {
-  	var post = response.feed.entry[0];
-
-   
-    var countpsy = document.getElementById("endposta").innerHTML =post.title.$t;
-
-if (countpsy.length >= '18'){
-
-document.getElementById("endpswh").innerHTML = 'رائع العنوان عدد الأحرف مناسب'
- document.getElementById("endpswh").style.color = 'green'
-if (countpsy.length <= '20'){
-
-document.getElementById("endpswh").innerHTML = 'سئ عدد أحرف العنوان قصيرة'
- document.getElementById("endpswh").style.color = 'red'
-}
-
-}
-
-      }
